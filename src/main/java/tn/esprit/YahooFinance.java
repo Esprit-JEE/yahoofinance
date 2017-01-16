71aa1a00cba1220c7f12682588421c3fc102136b
Walid-YAICH <wyaich@outlook.fr>
2017-01-16 18:20:30 +0100

    add git link

--- src/main/java/tn/esprit/YahooFinance.java
+++ src/main/java/tn/esprit/YahooFinance.java
@@ -12,11 +12,11 @@ import org.json.JSONObject;
 /**
  * @author Walid-YAICH
  * 
- * URL de yahoo :
+ * URL utiles :
  * https://developer.yahoo.com/yql/console/?q=show%20tables&env=store://datatables.org/alltableswithkeys#h=select+*+from+yahoo.finance.xchange+where+pair+in+(%22USD%22%2C+%22EUR%22)
- * 
  * http://theoryapp.com/parse-json-in-java/
  * https://jsonformatter.curiousconcept.com/
+ * https://github.com/Esprit-JavaEE/yahoofinance.git
  * 
  *
  */
